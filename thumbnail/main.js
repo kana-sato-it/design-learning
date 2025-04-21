c7f5e25e52f7227a71af01a0e176315e79e47088
Kana Sato <kanasato.it@gmail.com>
2025-04-21 22:35:45 +0900

    Add initial HTML, CSS, and JavaScript files for thumbnail gallery

---
+++ thumbnail/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function () {
+    var thumbs = $(".thumb img");
+    var top_images = $(".top img");
+
+    thumbs.hover(function () {
+      top_images
+        .hide()
+        .eq(thumbs.index($(this)))
+        .show();
+    });
+  });
+})(jQuery);
